f60e28fac86727d87b6693110db75fac3e81c8e8
ncdncd <arka.socky@gmail.com>
2023-06-25 19:27:52 +0700

    add constraints

---
+++ migrations/20230625121201-add-constraints-unique-cascade-allownull.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint("Users", {
+      type: "unique",
+      fields: ["username", "email", "phone"],
+    });
+    await queryInterface.addConstraint("Users", {
+      allowNull: false,
+      fields: ["username", "email", "phone"],
+    });
+    await queryInterface.addConstraint("Blogs", {
+      fields: ["authorId"],
+      type: "foreign key",
+      name: "Blogs_1",
+      references: {
+        table: "Users",
+        field: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+    await queryInterface.addConstraint("Likes", {
+      fields: ["userId"],
+      type: "foreign key",
+      name: "Likes_1",
+      references: {
+        table: "Users",
+        field: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+    await queryInterface.addConstraint("Likes", {
+      fields: ["blogId"],
+      type: "foreign key",
+      name: "Likes_2",
+      references: {
+        table: "Blogs",
+        field: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint("Users", {
+      type: "unique",
+      fields: ["username", "email", "phone"],
+    });
+    await queryInterface.removeConstraint("Users", {
+      allowNull: true,
+      fields: ["username", "email", "phone"],
+    });
+    await queryInterface.removeConstraint("Blogs", "Blogs_1");
+    await queryInterface.removeConstraint("Likes", "Likes_1");
+    await queryInterface.removeConstraint("Likes", "Likes_2");
+  }
+};
